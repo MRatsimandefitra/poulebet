680d8bde1d34af12a6d8d13be814fd2b40e54daa
fymatelli <fy@matelli.mg>
2016-08-08 17:40:08 +0300

    dernier commit

---
+++ src/Api/DBBundle/Form/AdresseSocieteType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Api\DBBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdresseSocieteType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('adresse', TextareaType::class, array('attr' => array('class' => 'adresse')));
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Api\DBBundle\Entity\AdresseSociete'
+        ));
+    }
+}
