6d6b814ab5877ff6a09b48817318434cb4c994d9
miora.ratsimandefitra <miora.ratsimandefitra@gmail.com>
2016-08-23 09:31:33 +0300

    Commit

--- web/js/utilisateur.js
+++ web/js/utilisateur.js
@@ -11,4 +11,21 @@ $(document).ready(function(){
         }
         return true;
     });
+
+
+
+
 });
+function supprimer(msg){
+    var response = confirm(msg);
+    if(!response){
+        return false;
+    }
+    return true;
+}
+function onSelectChange(value){
+    document.getElementById('nbpage').submit();
+    xhttp.open("GET", null, false);
+    xhttp.send();
+
+}
