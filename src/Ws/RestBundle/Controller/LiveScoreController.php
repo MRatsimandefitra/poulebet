ae9bbb357aa34343c2d9e07afa6379ea3bf65f20
fymatelli <fy@matelli.mg>
2016-09-06 14:43:19 +0300

    okok

--- src/Ws/RestBundle/Controller/LiveScoreController.php
+++ src/Ws/RestBundle/Controller/LiveScoreController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
 class LiveScoreController extends ApiController
 {
     const ENTITY_MATCHS = 'ApiDBBundle:Matchs';
@@ -18,26 +19,14 @@ class LiveScoreController extends ApiController
      * @param Request $request
      * @return JsonResponse
      */
-    public function getMatchLiveScoreAction(Request $request)
+    public function getMatchLiveScoreOldAction(Request $request)
     {
         $data = $this->getRepo(self::ENTITY_MATCHS)->getMatchLiveScore();
         $result = array();
         if ($data) {
-
-            /*    foreach ($data as $k => $vData) {
-
-                    $result['list_match'][] = array(
-                             $vData->getChampionat()->getNomChampionat() =>array(
-                                 'teamsDomicile' => $vData->getTeamsDomicile()->getFullNameClub(),
-                                 'teamsVisiteur' => $vData->getTeamsVisiteur()->getFullNameClub(),
-                                 'score' => $vData->getScore(),
-                                 'scoreDomicile' => substr($vData->getScore(), 0, 1),
-                                 'scoreVisiteur' => substr($vData->getScore(), -1, 1),
-                                 'liveTime' => '',
-                             )
-                    );
-                }*/
             $ancienNameChampionat = "";
+            $dataDetails= array();
+            $dataDetailsFull = array();
             foreach ($data as $k => $vData) {
 
                 if ($ancienNameChampionat != $vData->getChampionat()->getNomChampionat()) {
@@ -54,24 +43,32 @@ class LiveScoreController extends ApiController
                 $query = $this->get('doctrine.orm.entity_manager')->createQuery($dql);
                 $query->setParameter('vchampionat', $vChampionat);
                 $dataMatchs = $query->getResult();
-                /*$data['championat'][] = $vChampionat;*/
-               /* foreach ($dataMatchs as $kDataMatchs => $vDataMatchs) {
-                    //var_dump($vDataMatchs->getTeamsDomicile()->getFullNameClub()); die;
-                    $dataDetails['group'][]['championat'][$vDataMatchs->getChampionat()->getFullNameChampionat()][] = array(
-                        'teamsDomicile' => $vDataMatchs->getTeamsDomicile()->getFullNameClub(),
-                        'teamsVisiteur' => $vDataMatchs->getTeamsVisiteur()->getFullNameClub(),
-                        'score' => $vDataMatchs->getScore(),
-                        'scoreDomicile' => substr($vDataMatchs->getScore(), 0, 1),
-                        'scoreVisiteur' => substr($vDataMatchs->getScore(), -1, 1),
-                        'liveTime' => '',
-                    );
-                }*/
-                foreach($dataMatchs as $kDataMatchs => $vDataMatchs){
-                    $dataDetails['group'][] = array(
-                        'id' => $vDataMatchs->getChampionat()->getId(),
-                        'nom' => $vDataMatchs->getChampionat()->getFullNameChampionat()
-                    );
-                    $dataDetails['matchs'][] = array(
+
+                foreach ($dataMatchs as $kDataMatchs => $vDataMatchs) {
+                    //var_dump($vDataMatchs->getChampionat()->getId()); //break;
+                   // var_dump($dataDetailsFull['group']);
+             /*       if(sizeof($dataDetailsFull['group']) > 0 ){*/
+
+                        /*foreach($dataDetailsFull['group'] as $groupItems){
+                            var_dump($groupItems); die;
+                            if(array_key_exists('id', $groupItems) ){*/
+                             /*   if(array_key_exists('group', $dataDetailsFull) && !in_array($vDataMatchs->getChampionat()->getId(), $dataDetailsFull['group'])) {*/
+                /*                    $dataDetails[] = array(
+                                        'id' => $vDataMatchs->getChampionat()->getId(),
+                                        'nom' => $vDataMatchs->getChampionat()->getFullNameChampionat()
+                                    );
+                */                /*}*/
+                            /*}
+                        }*/
+                    /*}*/
+                    /*if(!in_array($vDataMatchs->getChampionat()->getId(), $dataDetailsFull['group'])) {
+                        $dataDetailsFull['group'][] = array(
+                            'id' => $vDataMatchs->getChampionat()->getId(),
+                            'nom' => $vDataMatchs->getChampionat()->getFullNameChampionat()
+                        );
+                    }*/
+
+                    $dataDetailsFull['matchs'][] = array(
                         'teamsDomicile' => $vDataMatchs->getTeamsDomicile()->getFullNameClub(),
                         'teamsVisiteur' => $vDataMatchs->getTeamsVisiteur()->getFullNameClub(),
                         'score' => $vDataMatchs->getScore(),
@@ -93,8 +90,18 @@ class LiveScoreController extends ApiController
                     }*/
                 }
 
+/*                foreach($dataDetails['group'] as $k => $v){
+                    if(!in_array($v['id'],$dataDetailsFull['group'][$k])){
+                        $dataDetailsFull['group'][] = array(
+                            'id' => $vDataMatchs->getChampionat()->getId(),
+                            'nom' => $vDataMatchs->getChampionat()->getFullNameChampionat()
+                        );
+                    }
+                }*/
+
             }
-            $result = $dataDetails;
+
+            $result = $dataDetailsFull;
             $result['code_error'] = 4;
             $result['success'] = true;
             $result['error'] = false;
@@ -107,5 +114,63 @@ class LiveScoreController extends ApiController
         }
 
         return new JsonResponse($result);
+    }
+
+
+    public function getMatchLiveScoreAction(Request $request){
+
+        $dqlChampionat = "SELECT m from ApiDBBundle:Matchs m
+                LEFT JOIN m.championat ch
+                where m.dateMatch BETWEEN CURRENT_DATE() AND DATE_ADD(CURRENT_DATE(), 7, 'day' )
+                AND m.statusMatch LIKE 'active' GROUP BY ch.fullNameChampionat ";
+        $queryChampionat = $this->get('doctrine.orm.entity_manager')->createQuery($dqlChampionat);
+        $matchLiveChampionat = $queryChampionat->getResult();
+        foreach($matchLiveChampionat as $kLiveChampionat => $vLiveChampionatItems){
+
+            $championat[] = array(
+                'id' => $vLiveChampionatItems->getChampionat()->getId(),
+                'nom' => $vLiveChampionatItems->getChampionat()->getFullNameChampionat()
+            );
+        }
+        $dataDetailsFull['group'] = $championat;
+        $dqlMatchs = "SELECT m from ApiDBBundle:Matchs m
+                LEFT JOIN m.championat ch
+                where m.dateMatch BETWEEN CURRENT_DATE() AND DATE_ADD(CURRENT_DATE(), 7, 'day' )
+                AND m.statusMatch LIKE 'active'";
+        $queryMatchs = $this->get('doctrine.orm.entity_manager')->createQuery($dqlMatchs);
+        $matchLive = $queryMatchs->getResult();
+
+        foreach($matchLive as $k => $matchLiveItems){
+            $dataDetailsFull['matchs'][] = array(
+                'teamsDomicile' => $matchLiveItems->getTeamsDomicile()->getFullNameClub(),
+                'teamsVisiteur' => $matchLiveItems->getTeamsVisiteur()->getFullNameClub(),
+                'score' => $matchLiveItems->getScore(),
+                'scoreDomicile' => substr($matchLiveItems->getScore(), 0, 1),
+                'scoreVisiteur' => substr($matchLiveItems->getScore(), -1, 1),
+                'live' => ($matchLiveItems->getStatusMatch() == 'active') ? true : false,
+                'current-state' => array(
+                    'period' => $matchLiveItems->getPeriod(),
+                    'minute' => $matchLiveItems->getMinute()
+                ),
+                'championat' => $matchLiveItems->getChampionat()->getId()
+
+            );
+        }
+        $result = $dataDetailsFull;
+        if(!empty($result)){
+            $result = $dataDetailsFull;
+            $result['code_error'] = 4;
+            $result['success'] = true;
+            $result['error'] = false;
+            $result['message'] = "Aucun matchs n'a été trouvé";
+        }else{
+            $result['code_error'] = 0;
+            $result['success'] = false;
+            $result['error'] = true;
+            $result['message'] = "Aucun matchs n'a été trouvé";
+        }
+        return new JsonResponse($result);
+
+
     }
 }
