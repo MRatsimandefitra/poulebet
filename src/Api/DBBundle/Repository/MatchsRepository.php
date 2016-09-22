deddac99608dad692af72ffeefb2c25cb43e1563
fymatelli <fy@matelli.mg>
2016-09-22 10:51:07 +0300

    okok

--- src/Api/DBBundle/Repository/MatchsRepository.php
+++ src/Api/DBBundle/Repository/MatchsRepository.php
@@ -90,7 +90,7 @@ class MatchsRepository extends \Doctrine\ORM\EntityRepository
                     WHERE m.dateMatch BETWEEN CURRENT_DATE() AND DATE_ADD(CURRENT_DATE(), 7, 'day')
                     AND ((m.masterProno1 is not null and m.masterProno1 = true) or (m.masterProno2 is not null and m.masterProno2 = true) or (m.masterPronoN is not null and m.masterPronoN = true))
                     AND   ch.nomChampionat LIKE :championat
-                    ORDER BY m.dateMatch ASC, m.id ASC, ch.rang ASC";
+                    ORDER BY m.dateMatch ASC, m.id ASC";
             //AND (m.masterProno1 is not null or m.masterProno2 is not null or m.masterPronoN is not null)
         }else{
 
