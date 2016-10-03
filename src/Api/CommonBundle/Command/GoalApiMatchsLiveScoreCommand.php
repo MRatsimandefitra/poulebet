152ab6b7994a308d69295495c87a69e4c25b163a
fymatelli <fy@matelli.mg>
2016-10-03 14:21:49 +0300

    okok

--- src/Api/CommonBundle/Command/GoalApiMatchsLiveScoreCommand.php
+++ src/Api/CommonBundle/Command/GoalApiMatchsLiveScoreCommand.php
@@ -296,8 +296,8 @@ class GoalApiMatchsLiveScoreCommand extends ContainerAwareCommand
     private function getMessagePush($vEventItems, $matchs){
         /*$msg = " <img src='". "http:dplb.arkeup.com/".$matchs->getCheminLogoDomicile() ."'width='15' height='15' /> <b>". $matchs->getEquipeDomicile()->getFullNameClub() ."</b> VS  <img src='".$matchs->getCheminLogoVisiteur()."' width='15' height='15' />  <b> ". $matchs->getEquipeVisiteur()->getFullNameClub() ."</b> <br />";*/
         $msg = "".$matchs->getEquipeDomicile().' VS '. $matchs->getEquipeVisiteur(). "$";
-        $msg .= " But de ".$vEventItems['player']. " à la " . $vEventItems['minute']. "$";
-        $msg .= " Score " . $vEventItems['score'];
+        $msg .= "But de ".$vEventItems['player']. " à la " . $vEventItems['minute']. "$";
+        $msg .= "Score " . $vEventItems['score'];
 
         $msg2 = "But de " .$vEventItems['player']. " à la " . $vEventItems['minute']. " $ - " . $matchs->getEquipeDomicile().' VS '. $matchs->getEquipeVisiteur(). " \n "." Score " . $vEventItems['score'];
         return $msg;
