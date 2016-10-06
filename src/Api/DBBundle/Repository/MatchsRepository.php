7bffa7c62a52240a9e45ded7ac9570fe15aff077
fymatelli <fy@matelli.mg>
2016-10-06 17:12:01 +0300

    okok

--- src/Api/DBBundle/Repository/MatchsRepository.php
+++ src/Api/DBBundle/Repository/MatchsRepository.php
@@ -231,7 +231,7 @@ class MatchsRepository extends \Doctrine\ORM\EntityRepository
 
         $params = array();
         $where = array();
-        $where[] = " m.dateMatch BETWEEN co.dateDebut AND co.dateFinale";
+        $where[] = " m.dateMatch >= co.dateDebut AND m.dateMatch <= co.dateFinale";
         $where[] = " co.id = :idConcour";
         $params['idConcour'] = $idConcour;
         if($date){
