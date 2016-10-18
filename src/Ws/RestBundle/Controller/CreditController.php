81463c8d5395abb70a138296ee22dc61c4b9efd2
fymatelli <fy@matelli.mg>
2016-10-18 14:24:47 +0300

    okok

--- src/Ws/RestBundle/Controller/CreditController.php
+++ src/Ws/RestBundle/Controller/CreditController.php
@@ -29,16 +29,19 @@ class CreditController extends ApiController implements InterfaceDB
         if(!$user){
             return $this->noUser();
         }
-
         $lastSolde = $this->getRepo(self::ENTITY_MVT_CREDIT)->findLastSolde($user->getId());
-        var_dump($lastSolde); die;
         if(is_object($lastSolde[0])){
             $idLast =  $lastSolde[0];
         }else{
             $idLast = $lastSolde[0][1];
         }
-        $mvtCreditLast = $this->getObjectRepoFrom(self::ENTITY_MVT_CREDIT, array('id' => $idLast));
-        $result['solde'] = $mvtCreditLast->getSoldeCredit();
+        if($idLast == null){
+            $result['solde'] = 0;
+        }else{
+            $mvtCreditLast = $this->getObjectRepoFrom(self::ENTITY_MVT_CREDIT, array('id' => $idLast));
+            $result['solde'] = $mvtCreditLast->getSoldeCredit();
+        }
+
         $result['code_error'] = 0;
         $result['success'] = true;
         $result['message'] = "Success";
