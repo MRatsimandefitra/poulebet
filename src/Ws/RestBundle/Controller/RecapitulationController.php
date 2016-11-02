28f67e04f1da47c675d8a9c3f6093c1a67bf7ddc
fymatelli <fy@matelli.mg>
2016-11-02 14:43:27 +0300

    okok

--- src/Ws/RestBundle/Controller/RecapitulationController.php
+++ src/Ws/RestBundle/Controller/RecapitulationController.php
@@ -72,7 +72,7 @@ class RecapitulationController extends ApiController implements InterfaceDB
 
                 foreach ($idMise as $k => $itemsIdMise) {
                     $count = $count + 1;
-                    if ($countRow == $k && $count < $countRow) {
+                    if ($countRow == $k && $countRow < $countTotalRow) {
                         $matchs = array();
                         $countRow= $countRow + 1;
                         $ss = $this->getRepo(self::ENTITY_MATCHS)->findMatchsForRecapCombined($user->getId(), $itemsIdMise);
