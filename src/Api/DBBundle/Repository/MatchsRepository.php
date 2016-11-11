3dbb0cd79c50e7f7eaba8a8896f74733a0b5493b
miora.ratsimandefitra <miora.ratsimandefitra@gmail.com>
2016-11-11 16:13:06 +0300

    Test

--- src/Api/DBBundle/Repository/MatchsRepository.php
+++ src/Api/DBBundle/Repository/MatchsRepository.php
@@ -236,7 +236,7 @@ class MatchsRepository extends \Doctrine\ORM\EntityRepository
 
         //$where[] = " m.dateMatch BETWEEN co.dateDebut AND co.dateFinale";
         $where[] = " co.id = :idConcour";
-        $where[] = " m.status_match != 'finished' ";
+        $where[] = " m.statusMatch != 'finished' ";
         $params['idConcour'] = $idConcour;
         if($date){
 
