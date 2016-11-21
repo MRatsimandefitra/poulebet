7108720c585a164ff3c5ab0187282389979103e8
miora.ratsimandefitra <miora.ratsimandefitra@gmail.com>
2016-11-21 15:46:38 +0300

    test

--- src/Api/DBBundle/Repository/MatchsRepository.php
+++ src/Api/DBBundle/Repository/MatchsRepository.php
@@ -378,7 +378,7 @@ class MatchsRepository extends \Doctrine\ORM\EntityRepository
     public function findMatchsForRecapCombined($idUser, $idMise){
         $dql = "SELECT vu from ApiDBBundle:VoteUtilisateur vu LEFT JOIN vu.matchs m LEFT JOIN vu.utilisateur u LEFT JOIN m.championat ch
                 WHERE u.id = :idUser And vu.idMise = :idMise 
-                ORDER BY vu.id ASC";
+                ORDER BY vu.id DESC";
         
         $query = $this->getEntityManager()->createQuery($dql);
         $query->setParameters(array('idUser' => $idUser, 'idMise' => $idMise));
@@ -402,7 +402,7 @@ class MatchsRepository extends \Doctrine\ORM\EntityRepository
     }
     public function findNbMatchsForRecapCombined($idUser){
         $dql = "SELECT vu from ApiDBBundle:VoteUtilisateur vu LEFT JOIN vu.matchs m LEFT JOIN vu.utilisateur u
-                WHERE u.id = :idUser AND vu.isCombined = true GROUP BY vu.idMise ";
+                WHERE u.id = :idUser AND vu.isCombined = true ORDER BY vu.id DESC GROUP BY vu.idMise";
         $query = $this->getEntityManager()->createQuery($dql);
         $query->setParameter('idUser' , $idUser);
         return $query->getResult();
