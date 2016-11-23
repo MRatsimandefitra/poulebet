52848eeec2c2685aaad654891340edc428b64855
miora.ratsimandefitra <miora.ratsimandefitra@gmail.com>
2016-11-23 11:16:16 +0300

    test

--- src/Api/DBBundle/Repository/MatchsRepository.php
+++ src/Api/DBBundle/Repository/MatchsRepository.php
@@ -657,13 +657,9 @@ class MatchsRepository extends \Doctrine\ORM\EntityRepository
 
     public function findTotalGainsOfUser($userId){
         $dql = "SELECT vu from ApiDBBundle:VoteUtilisateur vu
-                LEFT JOIN vu.matchs m
-                JOIN m.concours co
-                LEFT JOIN m.championat ch
                 LEFT JOIN vu.utilisateur u
                 WHERE vu.id = :userId
                 AND vu.gagnant = true
-                AND co.dateFinale < CURRENT_DATE()
                 GROUP BY vu.idMise";
         $query = $this->getEntityManager()->createQuery($dql);
         $query->setParameter('userId' , $userId);
